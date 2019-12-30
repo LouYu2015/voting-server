7d76a9b37dfb7652be4b373ce6bd493a78c18010
Tom Lou <louyu27@cs.washington.edu>
2019-12-29 20:08:55 -0800

    Allow cross origion access

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'voting.apps.VotingConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -120,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Cross origion access
+
+CORS_ORIGIN_WHITELIST = [
+    "https://vote.cssauw.org",
+    "http://localhost:3000"
+]
