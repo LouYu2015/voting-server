1adeb42ee8bfb00a27431b3ecfc477bc0d33b0f5
Tom Lou <louyu27@cs.washington.edu>
2021-06-28 17:36:55 -0700

    Add comments

--- voting/urls.py
+++ voting/urls.py
@@ -4,12 +4,12 @@ from .views import question_list_view, vote_count_view, update_vote_view, get_se
     enable_serial_number, disable_serial_number
 
 urlpatterns = [
-    path('questions/', question_list_view),
-    path('vote_count/<int:id>/', vote_count_view),
-    path('update_vote/<int:question_id>/', update_vote_view),
-    path('get_selected/<str:serial_number>/', get_selected_view),
-    path('enable/', enable_serial_number),
-    path('disable/', disable_serial_number)
+    path('questions/', question_list_view),  # Get a list of questions
+    path('vote_count/<int:id>/', vote_count_view),  # Get vote count for a question
+    path('update_vote/<int:question_id>/', update_vote_view),  # Update a user's vote for a question
+    path('get_selected/<str:serial_number>/', get_selected_view),  # Get the choice selected by the user
+    path('enable/', enable_serial_number),  # Enable questions
+    path('disable/', disable_serial_number)  # Disable questions
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
